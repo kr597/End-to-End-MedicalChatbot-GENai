262cb1ac0c9298300407990f62e3f57d1b97875a
KrishnaChoubey23 <krishnakantchoubey2023@gmail.com>
2026-01-06 18:04:44 +0530

    Folder Structure Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Krishnakant_Choubey_CSE",
+    author_email="krishnakantchoubey2023@gmil.com",
+    packages=find_packages(),
+    install_requires=[]
+)
